136faab4813e168c8bedb847f4a5edb56605afed
Sonualam <sonualam1599@gmail.com>
2023-12-18 23:10:07 +0530

    style improved responsiveness

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -5,7 +5,7 @@ import NavChild from "./NavChild";
 function HamburgerMenu() {
   const [secShow, setSecShow] = useState(false);
   return (
-    <div className=" w-[80%]  bg-[--white]  text-[--very-dark-gray-blue]  absolute top-[120px]  left-8 rounded-lg p-6 flex flex-col items-center justify-start gap-10 text-center shadow-xl ">
+    <div className=" w-[80%]  bg-[--white]  text-[--very-dark-gray-blue]  absolute top-[120px]  left-8 rounded-lg p-6 flex flex-col items-center justify-start gap-10 text-center shadow-xl z-10 ">
       <div className="flex items-center justify-center gap-4">
         <p>Product</p>
         <img src={arrowDown} alt="/" />
@@ -18,10 +18,7 @@ function HamburgerMenu() {
         <p onClick={() => setSecShow(!secShow)}>Connect</p>
         <img src={arrowDown} alt="/" />
       </div>
-      <div>
-        {secShow && <NavChild />}
-        <div className="w-full my-2 h-[0.5px] bg-[--grayish-blue] "></div>
-      </div>
+      {secShow && <NavChild />}
 
       <div className="flex flex-col justify-center items-center gap-2 ">
         <button
