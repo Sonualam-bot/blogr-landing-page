0eec35f5f26b748a7e91aed7299299aea94b2255
Sonualam <sonualam1599@gmail.com>
2023-12-18 23:04:13 +0530

    style improved responsiveness

--- src/components/MidSection2.jsx
+++ src/components/MidSection2.jsx
@@ -4,9 +4,9 @@ import circles from "../images/bg-pattern-circles.svg";
 function MidSection2() {
   return (
     <div className="w-full md:h-[400px] sm:mt-[250px] flex md:flex-row sm:flex-col items-end md:justify-between sm:justify-start   bg-[--very-dark-gray-blue] rounded-tr-[90px] rounded-bl-[90px]  relative ">
-      <div className=" md:w-[800px] sm:w-[470px]  md:h-[400px] overflow-hidden  sm:mb-[190px] ">
+      <div className=" md:w-[800px] sm:w-[470px]  md:h-[400px] overflow-hidden  sm:mb-[40px] ">
         <img
-          className=" w-full  object-cover  md:scale-125 sm:scale-170 sm:ml-[28px]  sm:pb-[390px] sm:w-[80%] md:-ml-[107px]  md:pt-2  md:pb-[3000px]  "
+          className=" w-full  object-cover  md:scale-125 sm:scale-190 sm:ml-[56px]  sm:pb-[260px] sm:w-[80%] md:-ml-[107px]  md:pt-2  md:pb-[3000px]  "
           src={circles}
           alt="/"
         />
@@ -14,11 +14,11 @@ function MidSection2() {
       <div className="absolute sm:-top-[170px] md:-bottom-[110px] md:left-[120px] sm:left-[90px] ">
         <img className="  " src={phones} alt="/" />
       </div>
-      <div className=" flex flex-col justify-center absolute sm:mt-[0]  sm:m-[50px] sm:text-center sm:items-center top-[30%] md:left-[48%] gap-9 sm:z-10 ">
-        <h1 className="text-5xl sm:text-4xl text-[--white] font-semibold  top-[20px] font-Overpass ">
+      <div className=" flex flex-col justify-center absolute sm:mt-[0]  sm:m-[50px] sm:text-center sm:items-center sm:top-[40%] md:top-[30%] md:left-[48%] gap-9 sm:z-10 ">
+        <h1 className="text-5xl  text-[--white] font-semibold  top-[20px] font-Overpass ">
           State of the Art Infrastructure
         </h1>
-        <p className="w-[75%] text-[--white] leading-7  text-base font-Overpass ">
+        <p className="md:w-[75%] text-[--white] leading-7 sm:text-xl  md:text-base font-Overpass ">
           With reliability and speed in mind, worldwide data centers provide the
           backbone for ultra-fast connectivity. This ensures your site will load
           instantly, no matter where your readers are, keeping your site
